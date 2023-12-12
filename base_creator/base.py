499e822c7f1ca4a3c87bf633b010c661fbb7447b
Robert Kaye <rob@musicbrainz.org>
2023-12-12 12:32:08 +0100

    Start work on the circular image

--- base_creator/base.py
+++ base_creator/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from colorsys import hsv_to_rgb
+from math import sin, cos, radians
 
 from PIL import Image
 import numpy as np
@@ -35,4 +36,21 @@ def create_rectangular_base_image(png_file, width, height, max_sv):
     save_image(png_file, width, height, bitmap)
 
 
-create_rectangular_base_image("base.png", width=200, height=148, max_sv=.7)
+def create_circular_base_image(png_file, radius, max_sv):
+
+    dia = radius * 2
+    print(dia)
+    bitmap = [ [ (0, 0, 0, 0) for x in range(dia) ] for y in range(dia) ]
+
+    for r in range(radius):
+        for t in range(3600):
+            x = int((cos(radians(t/10)) * r) + radius)
+            y = int((sin(radians(t/10)) * r) + radius)
+
+            rgb = hsv_to_rgb(t / 360, 1.0, 1.0)
+            bitmap[y][x] = (int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255), 255)
+
+    save_image(png_file, dia, dia, bitmap, "RGBA")
+
+#create_rectangular_base_image("base.png", width=200, height=148, max_sv=.7)
+create_circular_base_image("base.png", 500, max_sv=.7)
